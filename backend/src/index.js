261366dd44e6f472b85ffab4dd4dd85de5a68ad4
SaadIrshadd <saad2005irshadd@gmail.com>
2025-10-08 17:30:43 +0500

    Fixed CORS for frontend with credentials

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,12 +18,13 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
-app.use(
-    cors({
-        origin: "*",
-        credentials: true
-    })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",               // for local dev
+    "https://chatt-app-mern.vercel.app"    // your deployed frontend
+  ],
+  credentials: true,
+}));
 
 
 //ROUTES
